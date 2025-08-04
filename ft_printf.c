7dbe4a70444b4931a0beba3adbe8b5085b7f3f28
Qian Yu Foo <qfoo@u91z02s02.42kl.edu.my>
2025-08-04 18:02:44 +0800

    solved count(?)

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: qfoo <qfoo@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 19:58:33 by qfoo              #+#    #+#             */
-/*   Updated: 2025/08/04 17:42:00 by qfoo             ###   ########.fr       */
+/*   Updated: 2025/08/04 18:02:19 by qfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,26 @@
 
 int	check_specifier(char c, va_list arg_list)
 {
+	int	count;
+
+	count = 0;
 	if (c == 'c')
-		ft_putchar((char)va_arg(arg_list, int));
+		count = ft_putchar((char)va_arg(arg_list, int));
 	else if (c == 's')
-		ft_putstr(va_arg(arg_list, char *));
+		count = ft_putstr(va_arg(arg_list, char *));
 	else if (c == 'p')
-		ft_putptr(va_arg(arg_list, void *));
+		count = ft_putptr(va_arg(arg_list, void *));
 	else if (c == 'd' || c == 'i')
-		ft_putnbr(va_arg(arg_list, int));
+		count = ft_putnbr(va_arg(arg_list, int));
 	else if (c == 'u')
-		ft_putui(va_arg(arg_list, unsigned int));
+		count = ft_putui(va_arg(arg_list, unsigned int));
 	else if (c == 'x')
-		ft_puthex(va_arg(arg_list, unsigned int), c);
+		count = ft_puthex(va_arg(arg_list, unsigned int), c);
 	else if (c == 'X')
-		ft_puthex(va_arg(arg_list, unsigned int), c);
+		count = ft_puthex(va_arg(arg_list, unsigned int), c);
 	else if (c == '%')
-		ft_putchar('%');
-	// else
-	// 	ft_putchar(c);
-	return (0);
+		count = ft_putchar('%');
+	return (count);
 }
 
 int ft_printf(const char *format, ...)
@@ -59,19 +60,14 @@ int ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			check_specifier(format[i + 1], arg_list);
+			count += check_specifier(format[i + 1], arg_list);
 			i++;
-			count--;
 		}
 		else
-		{
-			ft_putchar(format[i]);
-			count++;
-		}
+			count += ft_putchar(format[i]);
 		i++;
 	}
 	va_end(arg_list);
-	count += i;
 	return (count);
 }
 
